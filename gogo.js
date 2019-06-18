5b7be8c5adcd35e6e964d8973164f846b859e97f
Linda <myy9805@postech.ac.kr>
2019-06-18 14:15:50 +0900

    edited

--- gogo.js
+++ gogo.js
@@ -1,15 +1,20 @@
 const err = new Error('invalid email'); // 이 인스턴스는 에러와 통신하는 수단.
 function validateEmail(email){
-	return email.match(/@/)?email:new Error('invalid email: ${email}');
+	return email.match(/@/)?email:new Error(`invalid email: ${email}`);
 }
 
-const email='myy9805@postech.ac.kr';
+const email=null;
 
+
+try{
 const validated=validateEmail(email);
 
 if(validated instanceof Error){
-	console.error("Error: ${validated.message}");
+	console.error(`Error_${validated.message}`);
 }
 else{
-	console.log("OKay: ${validated}");
+	console.log(`OKay: ${validated}`);
+}
+}catch(err){
+	console.error(`Error_${err.message}`);
 }
